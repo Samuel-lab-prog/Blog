f98dd85c2290e90446e48353c66be33ab336661f
Samuel <sg1976387@gmail.com>
2025-11-18 19:39:03 -0300

    style: improved home page style

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   const posts = useFetchPosts(3);
   const user = useUser();
   return (
-    <main className="absolute left-0 top-20 flex flex-col w-full">
-      <section className="p-4 xl:p-12 h-70 flex flex-col justify-center items-start">
+    <main className="absolute left-0 top-20 flex flex-col w-full pb-12  ">
+      <section className="px-4 xl:px-12 h-70 flex flex-col justify-center items-start">
         <h5 className="text-gray-600">
           Bem-vindo(a) {user?.firstName} {user?.lastName}!
         </h5>
@@ -18,7 +18,7 @@ export default function Home() {
         </p>
 
       </section>
-      <section className="p-4 xl:p-12 flex flex-col items-start">
+      <section className="px-4 xl:px-12 flex flex-col items-start">
         <h2 className="my-4 lg:text-5xl">Últimos posts</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
           { posts.map((post) => (
